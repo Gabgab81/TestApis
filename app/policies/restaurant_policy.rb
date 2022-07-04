d7c9fbd5a7be32a2f9de3e22d2410afe8ca053c5
gabriel roux <rouxgab81@gmail.com>
2022-07-04 18:53:46 -0400

    pundit done

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,26 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+      # user.admin? ? scope.all : scope.where(user: user)
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user || user.admin
+  end
+
+  def destroy?
+    record.user == user || user.admin
+  end
+
+end
