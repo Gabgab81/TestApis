aaa33290a181761b5d871668c30f301dd76739ac
gabriel roux <rouxgab81@gmail.com>
2022-07-04 17:37:02 -0400

    reference restaurant done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,8 @@ class Restaurant < ApplicationRecord
     geocoded_by :address
     after_validation :geocode, if: :will_save_change_to_address?
 
+    belongs_to :user
+
     validates :name, presence: true
     validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_205024) do
+ActiveRecord::Schema.define(version: 2022_07_04_212124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_07_04_205024) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_07_04_205024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
